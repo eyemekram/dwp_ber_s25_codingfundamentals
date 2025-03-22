d6f3e620e434ab97fb61ef79b8a534df9c4aa999
Ekram Alomari <ekram.omari@gmail.com>
2025-03-22 12:21:14 +0000

    more class work

--- homework/lesson-3/classwork.py
+++ homework/lesson-3/classwork.py
@@ -1,11 +1,38 @@
-score =50 
-if score > 90 :
-    print ("Your grade is A")
-elif score > 80 and score <= 90 :
-    print ("Your grade is B")
-elif score >= 60 and score <= 80 :
-    print ("Your grade is C")
-elif score < 60 and score >= 50:
-    print ("Your grade is D")
-else:
-    print ("you have failed")
+# # Excercise 1
+# score =50 
+# if score > 90 :
+#     print ("Your grade is A")
+# elif score > 80 and score <= 90 :
+#     print ("Your grade is B")
+# elif score >= 60 and score <= 80 :
+#     print ("Your grade is C")
+# elif score < 60 and score >= 50:
+#     print ("Your grade is D")
+# else:
+#     print ("you have failed")
+
+# # Excercise 2
+# fruits = {"apple" , "banana", "orange", "grape"}
+# for fruit in fruits :
+#     print (fruit)
+
+# # Excercise 3
+
+# number = 1
+# while number <= 100:
+#     number = number + 2
+#     print (number)
+# # TODO: stop it at 99
+
+# for number in range (1,101) :
+#     print (number)
+
+# # Excercise 4
+# username = input ("What should we call you?")
+# print ("Hello", username + ", nice to meet you!")
+
+
+input2 ("What is your birth year?")
+input3 ("What is your favourite number?")
+lucky_number = input2 + input3
+print (lucky_number)
