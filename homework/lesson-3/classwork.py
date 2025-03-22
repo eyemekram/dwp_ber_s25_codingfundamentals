0d05e2fc2f30765c500747d5cecba8ed254e4dee
Ekram Alomari <ekram.omari@gmail.com>
2025-03-22 11:36:00 +0000

    I added classwork

---
+++ homework/lesson-3/classwork.py
@@ -0,0 +1,11 @@
+score =50 
+if score > 90 :
+    print ("Your grade is A")
+elif score > 80 and score <= 90 :
+    print ("Your grade is B")
+elif score >= 60 and score <= 80 :
+    print ("Your grade is C")
+elif score < 60 and score >= 50:
+    print ("Your grade is D")
+else:
+    print ("you have failed")
